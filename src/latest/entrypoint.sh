345dcc6a210ef4680f333db1051c8d1758ed4dd0
Steffen Graber <s.graber@fz-juelich.de>
2019-11-28 09:34:21 +0100

    fix nest --help

--- src/latest/entrypoint.sh
+++ src/latest/entrypoint.sh
@@ -13,7 +13,7 @@ echo '. /opt/nest/bin/nest_vars.sh' >> /home/nest/.bashrc
 source /opt/nest/bin/nest_vars.sh
 
 # Running NEST to test and to copy the .nestrc into /home/nest
-nest
+nest --help
 
 export MUSIC_ROOT_DIR=/opt/music-install
 export MUSIC_ROOT=${MUSIC_ROOT_DIR}
