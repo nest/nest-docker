ad6c1813d8cfe4dcd11b120d0e4b622223cf2f63
Christopher Bignamini <christopher.bignamini@gmail.com>
2021-07-27 16:50:18 +0200

    latest_daint option added

--- run.sh
+++ run.sh
@@ -48,12 +48,12 @@ fi
 case $command in
     provision)
         echo
-        echo "Provisioning needs an argument: 'latest' '2.12.0', '2.14.0',"
+        echo "Provisioning needs an argument: 'latest' 'latest_daint' '2.12.0', '2.14.0',"
         echo "'2.16.0', '2.18.0', '2.20.0', '2.20.1','3.0' or 'all'."
         echo
         while test $# -gt 0; do
             case "$1" in
-            latest | 2.12.0 | 2.14.0 | 2.16.0 | 2.18.0 | 2.20.0 | 2.20.1 | 3.0 )
+            latest | latest_daint | 2.12.0 | 2.14.0 | 2.16.0 | 2.18.0 | 2.20.0 | 2.20.1 | 3.0 )
                 echo "Build the NEST image for NEST $1"
                 echo
                 docker build -t nestsim/nest:"$1" ./src/"$1"
@@ -62,7 +62,7 @@ case $command in
                 ;;
             all)
                 echo "Build the NEST image for NEST 2.12.0, 2.14.0,"
-                echo "2.16.0, 2.18.0, 2.20.0, 2.20.0, 3.0 and latest"
+                echo "2.16.0, 2.18.0, 2.20.0, 2.20.0, 3.0, latest and latest_daint"
                 echo
                 docker build -t nestsim/nest:2.12.0 ./src/2.12.0
                 docker build -t nestsim/nest:2.14.0 ./src/2.14.0
@@ -72,6 +72,7 @@ case $command in
                 docker build -t nestsim/nest:2.20.1 ./src/2.20.1
                 docker build -t nestsim/nest:3.0 ./src/3.0
                 docker build -t nestsim/nest:latest ./src/latest
+                docker build -t nestsim/nest:latest ./src/latest_daint
                 echo
                 echo "Finished!"
                 ;;
@@ -92,14 +93,14 @@ case $command in
         echo "  - 'virtual VERSION'"
         echo
         echo "VERSION is the version of NEST"
-        echo "(e.g. latest, 2.12.0, 2.14.0, 2.16.0, 2.18.0, 2.20.0, 2.20.1, 3.0)"
+        echo "(e.g. latest, latest_daint, 2.12.0, 2.14.0, 2.16.0, 2.18.0, 2.20.0, 2.20.1, 3.0)"
         echo
     LOCALDIR="$(pwd)"
     while test $# -gt 1; do
         case "$1" in
             notebook)
                 case "$2" in
-                    latest | 2.12.0 | 2.14.0 | 2.16.0 | 2.18.0 | 2.20.0 | 2.20.1 | 3.0)
+                    latest | latest_daint | 2.12.0 | 2.14.0 | 2.16.0 | 2.18.0 | 2.20.0 | 2.20.1 | 3.0)
                     echo "Run NEST-$2 with Jupyter Notebook".
                     echo
                     docker run -it --rm -e LOCAL_USER_ID=`id -u $USER` --name my_app  \
@@ -115,7 +116,7 @@ case $command in
             ;;
             interactive)
                 case "$2" in
-                    latest | 2.12.0 | 2.14.0 | 2.16.0 | 2.18.0 | 2.20.0 | 2.20.1 | 3.0)
+                    latest | latest_daint | 2.12.0 | 2.14.0 | 2.16.0 | 2.18.0 | 2.20.0 | 2.20.1 | 3.0)
                     echo "Run NEST-$2 in interactive mode."
                     echo
                     docker run -it --rm -e LOCAL_USER_ID=`id -u $USER` --name my_app  \
@@ -131,7 +132,7 @@ case $command in
             ;;
             virtual)
                 case "$2" in
-                    latest | 2.12.0 | 2.14.0 | 2.16.0 | 2.18.0 | 2.20.0 |2.20.1 | 3.0)
+                    latest | latest_daint | 2.12.0 | 2.14.0 | 2.16.0 | 2.18.0 | 2.20.0 |2.20.1 | 3.0)
                     echo "Run NEST-$2 like a virtual machine."
                     echo
                     docker run -it --rm -e LOCAL_USER_ID=`id -u $USER` --name my_app  \
