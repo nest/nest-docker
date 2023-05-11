4e9d493f4bee217e55ff3d98ee3c862965b1a567
Steffen Graber <s.graber@fz-juelich.de>
2023-05-11 04:28:26 +0000

    Add Dockerfile

---
+++ src/2.14.1/entrypoint.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+set -e
+
+IP_ADDRESS=$(hostname --ip-address)
+USER_ID=${LOCAL_USER_ID:-9001}
+
+if [[ ! $(id -u nest) = $USER_ID ]]; then
+	echo "UID : $USER_ID"
+	adduser --disabled-login --gecos 'NEST' --uid $USER_ID --home /home/nest nest
+	export HOME=/home/nest
+fi
+
+echo '. /opt/nest/bin/nest_vars.sh' >> /home/nest/.bashrc
+
+# NEST environment
+source /opt/nest/bin/nest_vars.sh
+
+# Running NEST to test and to copy the .nestrc into /home/nest
+nest --help
+chown nest:nest /home/nest/.nestrc
+
+if [[ ! -d /opt/data ]]; then
+	mkdir /opt/data
+	chown -R nest:nest /opt/data
+fi
+
+if [[ "$1" = 'notebook' ]]; then
+    cd /opt/data
+    exec gosu nest jupyter-notebook --ip="${IP_ADDRESS}" --port=8080 --no-browser
+fi
+
+if [[ "$1" = 'interactive' ]]; then
+    read -p "Your python script: " name
+	echo Starting: $name
+	cd /opt/data
+	# Start
+	exec gosu nest python3 /opt/data/$name
+fi
+
+cd /opt/data
+exec gosu nest "$@"
