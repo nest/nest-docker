3d3b8acbd70fbe3fe9a90f2fa2c1cb898e60c3d8
Steffen Graber <s.graber@fz-juelich.de>
2023-06-28 10:04:42 +0000

    Add HDF5_DISABLE_VERSION_CHECK=1

--- src/3.5rc1/test-nest.sh
+++ src/3.5rc1/test-nest.sh
@@ -7,6 +7,7 @@ export OMPI_ALLOW_RUN_AS_ROOT_CONFIRM=1
 # install what is needed
 pip3 install pytest-xdist pytest-timeout junitparser
 
+HDF5_DISABLE_VERSION_CHECK=1
 
 cd /opt/nest/share/nest/testsuite/
-bash do_tests.sh --prefix=/opt/nest --report-dir=/opt/reports --with-python=/usr/bin/python
+bash do_tests.sh --prefix=/opt/nest --with-python=/usr/bin/python
