d19e8dad1e6d3a11382bb436afef3e58790ed391
Steffen Graber <s.graber@fz-juelich.de>
2023-07-05 08:13:02 +0200

    Fix path to hdf5

--- src/dev/entrypoint.sh
+++ src/dev/entrypoint.sh
@@ -16,9 +16,6 @@ export PATH=${MUSIC_PATH}/bin:$PATH
 export CPATH=${MUSIC_PATH}/include:$CPATH
 export PYTHONPATH=${MUSIC_PATH}/lib/python3.8/site-packages:$PYTHONPATH
 
-export PATH="$PATH:/opt/hdf5"
-
-
 MODE="${NEST_CONTAINER_MODE:-$1}"
 if [[ "${MODE}" = 'interactive' ]]; then
     read -p "Your python script: " name
