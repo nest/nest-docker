48d2a9d7664e40ab06fbc3c6dc57cc5fce123b9a
Steffen Graber <s.graber@fz-juelich.de>
2024-03-12 10:50:05 +0000

    Add 1st version of redo

--- src/dev/entrypoint.sh
+++ src/dev/entrypoint.sh
@@ -8,7 +8,7 @@ source /opt/nest/bin/nest_vars.sh
 # Running NEST to test and to copy the .nestrc into /home/nest
 nest --help
 
-export MUSIC_ROOT_DIR=/opt/music-install
+export MUSIC_ROOT_DIR='$HOME/.cache/music.install'
 export MUSIC_ROOT=${MUSIC_ROOT_DIR}
 MUSIC_PATH=${MUSIC_ROOT_DIR}
 export LD_LIBRARY_PATH=${MUSIC_PATH}/lib:$LD_LIBRARY_PATH
